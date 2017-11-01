33b668069d7698913be87c0198c93838cb82c8dc
Christopher Friedman <christopherfriedman@gmail.com>
2017-11-01 18:43:26 -0500

    we got turns

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,21 +1,23 @@
 class GamesController < ApplicationController
 
-
   def index
   end
 
   def show
-
     game = Game.find(params[:id])
-
     render locals: {game: game}
   end
 
-  def my_turn
+  def update
+
+    @game = Game.find_by(id: params[:id].to_i)
+    @game.turn_id = session[:user_id]
+    @game.save
+    redirect_to @game
+
   end
 
-  def im_done
-  end
+
 
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,6 @@ Rails.application.routes.draw do
 
   resources :games, :players
 
-  patch 'games/:id', to: 'games#my_turn'
 
 
 
